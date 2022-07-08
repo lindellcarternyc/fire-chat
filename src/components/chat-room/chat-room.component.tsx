04e41576cdbc111279047a9b119cbe92439000f0
lindellcarternyc <lindellcarternyc@gmail.com>
2022-07-08 14:45:34 -0400

    uses new button input component

--- src/components/chat-room/chat-room.component.tsx
+++ src/components/chat-room/chat-room.component.tsx
@@ -1,10 +1,16 @@
 import { Link, useParams } from 'react-router-dom'
+
 import { chatRooms } from '../../data/chat-rooms'
-import MessageInput from '../message-input'
+import { useAuth } from '../../hooks/use-auth'
+import { sendMessage } from '../../services/firebase.service'
+
+import ButtonInput from '../button-input'
 import MessageList from '../message-list'
+
 import classes from './chat-room.module.css'
 
 const ChatRoom = () => {
+  const user = useAuth().user!
   const { id } = useParams<{ id: string }>()
   const room = chatRooms.find(x => x.id === id)
 
@@ -31,7 +37,17 @@ const ChatRoom = () => {
       </div>
       <div className={classes['messages-container']}>
         <MessageList roomId={room.id} />
-        <MessageInput roomId={room.id} />
+        <ButtonInput 
+          input={{
+            placeholder: 'Enter a message'
+          }}
+          submit={{
+            text: 'Send',
+            handler: (text) => {
+              sendMessage(room.id, user, text)
+            }
+          }}
+        />
       </div>
     </>
   )
