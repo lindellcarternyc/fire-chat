f35686e6948a1832f8477f46e801303ef8d110c5
lindellcarternyc <lindellcarternyc@gmail.com>
2022-07-08 15:07:57 -0400

    adds support for adding a new room

--- src/components/landing/landing.component.tsx
+++ src/components/landing/landing.component.tsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom'
+
 import { useRooms } from '../../hooks/use-rooms'
+import { createRoom } from '../../services/firebase.service'
+
+import ButtonInput from '../button-input'
+
 import classes from './landing.module.css'
 
 const Landing = () => {
@@ -8,6 +13,15 @@ const Landing = () => {
   return (
     <>
       <h2>Choose a Chat Room</h2>
+      <ButtonInput 
+        input={{
+          placeholder: 'Add a new room?'
+        }}
+        submit={{
+          text: 'Add',
+          handler: createRoom
+        }}
+      />
       <ul className={classes['chat-room-list']}>
         {rooms.map(room => (
           <li key={room.id}>
